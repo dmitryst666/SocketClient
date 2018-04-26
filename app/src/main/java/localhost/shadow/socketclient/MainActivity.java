f09df29d2efc63b872a65e2a014de91a7e622afa
dmitryst666 <dmitryst666@gmail.com>
2018-04-26 15:23:49 +0400

    Layout added

--- app/src/main/java/localhost/shadow/socketclient/MainActivity.java
+++ app/src/main/java/localhost/shadow/socketclient/MainActivity.java
@@ -2,16 +2,30 @@ package localhost.shadow.socketclient;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+    Button btnRefresh = (Button) findViewById(R.id.refresh);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Client client = new Client();
+        final Client client = new Client();
         client.setCommand("SELECT TOP 100 * FROM test;");
+
+        View.OnClickListener oclBtnRef = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                client.setCommand("SELECT GETDATE()");
+            }
+        };
+
+        btnRefresh.setOnClickListener(oclBtnRef);
 
     }
 }
