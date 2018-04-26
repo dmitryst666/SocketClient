fce7369af0b91d8821ee4221d451ef1db3be9604
dmitryst666 <dmitryst666@gmail.com>
2018-04-26 13:22:43 +0400

    IP changed

--- app/src/main/java/localhost/shadow/socketclient/MainActivity.java
+++ app/src/main/java/localhost/shadow/socketclient/MainActivity.java
@@ -9,5 +9,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Client client = new Client();
+        client.setCommand("SELECT TOP 100 * FROM test;");
+
     }
 }
